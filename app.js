c5f9dd3a52d131295cda93695435225b695e7b5e
breogann <fernandocostapradillo@gmail.com>
2022-03-26 16:59:49 +0100

    expressed

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-//create express thingy
+//Import & declare express
+const express = require("express");
 
-function hello() {
-  return "hello world";
-}
+const hbs = require("hbs");
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+//Handling functions
+app.get("/", (request, response) => {
+  response.render("home");
+});
+
+app.get("/home", (request, response) => {
+  response.render("home");
+});
+
+app.get("/about", (request, response) => {
+  response.render("about");
+});
+
+app.get("/works", (request, response) => {
+  response.render("works");
+});
+
+app.listen(3000);
