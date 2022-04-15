18298ff6d6199681f481d234a1c8cd33bf1bf06a
breogann <fernandocostapradillo@gmail.com>
2022-04-15 20:10:27 +0200

    routes okay, needs footer & contents

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
+// menu navegacion
+// footer abajo
+// bootstrap
+
 //Import & declare express
 const express = require("express");
-
 const hbs = require("hbs");
 const app = express();
+
 app.use(express.static("public"));
+
+// Express: use HBS as rendering
 app.set("view engine", "hbs");
+
+// Location of the partials
 hbs.registerPartials(__dirname + "/views/partials");
 
-//Handling functions
-app.get("/", (request, response) => {
-  response.render("home");
-});
+app.locals.pageTitle = "PS management";
 
-app.get("/home", (request, response) => {
-  response.render("home");
+//Handling functions
+app.get(["/", "/home"], (request, response) => {
+  response.render("home", { message: "Welcome to this website" });
 });
 
 app.get("/about", (request, response) => {
-  response.render("about");
+  response.render("about", { message: "About message" });
 });
 
 app.get("/works", (request, response) => {
-  response.render("works");
+  response.render("works", { message: "Whatever" });
 });
 
-app.listen(3000);
+app.listen(3000, () => "listening on port 3000");
